62c6e6e7729651f943126efd8af119b7379760f9
Kaati <kaatipuola@gmail.com>
2024-01-29 14:53:47 +0300

    Add brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from brain_games.general import general
+from brain_games.play_games import even
+
+def main():
+    general(even)
+
+if __name__ == '__main__':
+    main()
