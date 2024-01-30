a249bc18d8d566ae0954b240d158a571cf8e9609
Kaati <kaatipuola@gmail.com>
2024-01-30 15:10:05 +0300

    Update brain_calc.py, brain_even.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,8 +3,10 @@
 from brain_games.general import general
 from brain_games.games import calc
 
+
 def main():
     general(calc)
+
 
 if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 from brain_games.general import general
-from brain_games.play_games import even
+from brain_games.games import even
+
 
 def main():
     general(even)
+
 
 if __name__ == '__main__':
     main()
