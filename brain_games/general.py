11ec458297ade362aff42d343f98359c135e353e
Kaati <kaatipuola@gmail.com>
2024-01-30 14:02:10 +0300

    Update general.py

--- brain_games/general.py
+++ brain_games/general.py
@@ -2,19 +2,19 @@ import prompt
 ATTEMPTS = 3
 
 
-def general(play):
+def general():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(play.RULE)
+    print(RULE)
     for _ in range(ATTEMPTS):
-        question, right_answer = play.game()
+        question, right_answer = game()
         print(f"Question: {question}")
         answer = prompt.string('Your answer: ')
-        if answer == right_answer:
+        if right_answer == answer:
             print('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(."
+            print(f"'{answer}' is wrong answer ;(. "
                   f"Correct answer was '{right_answer}'.")
             print(f"Let's try again, {name}!")
             break
