3f122b491a67136d2e8c6d173276772e6c36718d
Kaati <kaatipuola@gmail.com>
2024-01-31 13:00:57 +0300

    Add scripts/brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.general import general
+from brain_games.games import prime
+
+
+def main():
+    general(prime)
+
+
+if __name__ == '__main__':
+    main()
