8cefd322feed3aac93cc9a4188146bbfdf941e56
Kaati <kaatipuola@gmail.com>
2024-02-02 01:24:59 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,11 +2,16 @@ from random import randint
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def game():
-    question = randint(2, 100)
+def is_prime(number):
     count = 0
-    for i in range(2, question // 2 + 1):
-        if question % i == 0:
-            count = count + 1
-    right_answer = 'yes' if count <= 0 else 'no'
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:
+            count += 1
+    return count
+
+
+def play_game():
+    question = randint(2, 100)
+    number = question
+    right_answer = 'yes' if is_prime(number) <= 0 else 'no'
     return question, right_answer
