11b630cd96faf70d0ab06e50498c2dcb556f9eac
Kaati <kaatipuola@gmail.com>
2024-02-02 01:22:38 +0300

    Update scripts/brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.general import general
+from brain_games.general import run_game
 from brain_games.games import gcd
 
 
 def main():
-    general(gcd)
+    run_game(gcd)
 
 
 if __name__ == '__main__':
