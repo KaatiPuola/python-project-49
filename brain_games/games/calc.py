c6ec9fc6a7e1069058950dbe4f431304d74fa17c
Kaati <kaatipuola@gmail.com>
2024-02-02 01:17:43 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,7 +3,7 @@ import random
 RULE = 'What is the result of the expression?'
 
 
-def game():
+def play_game():
     operators = ['+', '-', '*']
     number1, number2 = randint(1, 10), randint(1, 10)
     operator = random.choice(operators)
