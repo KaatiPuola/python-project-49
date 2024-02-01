a22870e4e90ced24d79f43baadb3f353eab23935
Kaati <kaatipuola@gmail.com>
2024-02-02 01:24:15 +0300

    Update scripts/brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.general import general
+from brain_games.general import run_game
 from brain_games.games import progression
 
 
 def main():
-    general(progression)
+    run_game(progression)
 
 
 if __name__ == '__main__':
