6107982922318b91e163826810dedac9fec978e6
Kaati <kaatipuola@gmail.com>
2024-02-02 01:23:30 +0300

    Update scripts/brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.general import general
+from brain_games.general import run_game
 from brain_games.games import prime
 
 
 def main():
-    general(prime)
+    run_game(prime)
 
 
 if __name__ == '__main__':
