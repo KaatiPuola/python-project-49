8eb264448948a825b26ba149ec21cd4d840c22a9
Kaati <kaatipuola@gmail.com>
2024-02-02 23:32:37 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,15 +3,14 @@ RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    count = 0
     for i in range(2, number // 2 + 1):
         if number % i == 0:
-            count += 1
-    return count
+            return False
+    return True
 
 
 def play_game():
     question = randint(2, 100)
     number = question
-    right_answer = 'yes' if is_prime(number) <= 0 else 'no'
+    right_answer = 'yes' if is_prime(number) else 'no'
     return question, right_answer
