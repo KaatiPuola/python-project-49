23368c1ee978553a10b0a1d98dd5591e085ddbf1
Kaati <kaatipuola@gmail.com>
2024-02-02 23:39:59 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,9 +5,9 @@ RULE = 'What number is missing in the progression?'
 def play_game():
     start, step = randint(1, 100), randint(2, 5)
     length = randint(5, 10)
-    prog_list = list(range(start, start + step * length, step))
-    random_index = randint(0, len(prog_list) - 1)
-    right_answer = prog_list[random_index]
-    prog_list[random_index] = '..'
-    question = ' '.join(str(item) for item in prog_list)
+    progression = list(range(start, start + step * length, step))
+    random_index = randint(0, len(progression) - 1)
+    right_answer = progression[random_index]
+    progression[random_index] = '..'
+    question = ' '.join(str(item) for item in progression)
     return question, str(right_answer)
