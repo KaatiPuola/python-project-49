614aef8681b112e0e6aa4f0f201baf66d07d6385
Kaati <kaatipuola@gmail.com>
2024-02-02 11:59:54 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,17 +1,13 @@
-import random
 from random import randint
 RULE = 'What number is missing in the progression?'
 
 
 def play_game():
-    step_progression = randint(2, 5)
-    list1 = list(range(1, 100, step_progression))
+    start, step = randint(1, 100), randint(2, 5)
     length = randint(5, 10)
-    idx = randint(0, len(list1) - length)
-    list2 = list1[idx:idx + length]
-    right_answer = random.choice(list2)
-    for index, value in enumerate(list2):
-        if value == right_answer:
-            list2[index] = '..'
-    question = ' '.join(str(item) for item in list2)
+    prog_list = list(range(start, start + step * length, step))
+    random_index = randint(0, len(prog_list) - 1)
+    right_answer = prog_list[random_index]
+    prog_list[random_index] = '..'
+    question = ' '.join(str(item) for item in prog_list)
     return question, str(right_answer)
